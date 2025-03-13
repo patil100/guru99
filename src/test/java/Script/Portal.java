328f4c361e807175246057949779a65c1e5a541c
swapnil <pswapnilpatil100@gmail.com>
2025-03-13 16:20:39 +0530

    first commit

--- src/test/java/Script/Portal.java
+++ src/test/java/Script/Portal.java
@@ -3,12 +3,16 @@ package Script;
 import static Report.AllureReport.SaveTextLog;
 import static Report.AllureReport.Screenshot;
 
+import Read_Write_File.ReadFromCSV;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
 import Function.Driver;
 import io.qameta.allure.*;
+
+import java.io.IOException;
 
 public class Portal {
 
@@ -36,14 +40,32 @@ public class Portal {
 		Screenshot(driver, "Opening URL");
 	}
 
-	@Test
-	public void APortal() {
-		String portalUrl = "https://cbuatportal.tourasuae.com/login";
-		System.out.println("Opening Portal URL: " + portalUrl);
-		openurl(portalUrl);
+	@Step("login ")
+	public void login(String username,String password){
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys(username);
+		driver.findElement(By.xpath("//*[@name=\"comfirm_password\"]")).sendKeys(password);
+
 	}
 
-	@AfterTest
+	@Test
+	public void APortal() throws Exception {
+		String Path = System.getProperty("user.dir") + "//Merchant_Credentails.csv";
+		ReadFromCSV csv1 = new ReadFromCSV(Path);
+		String [] credentials = csv1.ReadLineNumber(1);
+		openurl(credentials[0]);
+		login(credentials[1], credentials[2]);
+	}
+
+	@Test
+	public void Aortal() throws Exception {
+		String Path = System.getProperty("user.dir") + "//Merchant_Credentails.csv";
+		ReadFromCSV csv1 = new ReadFromCSV(Path);
+		String [] credentials = csv1.ReadLineNumber(1);
+		openurl(credentials[0]);
+		login(credentials[1], credentials[2]);
+	}
+
+	//@AfterTest
 	public void teardown() {
 		if (driver != null) {
 			driver.quit();
